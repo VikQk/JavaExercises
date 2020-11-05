c8a5caaeacae06e584ee497b590f64fb7c0fb120
mati <mati@DESKTOP-GAKM3OF>
2020-11-05 13:57:45 +0100

    actualizacion 05/11/2020

--- TestMAVENtoH2/src/main/java/app/Main.java
+++ TestMAVENtoH2/src/main/java/app/Main.java
@@ -20,30 +20,38 @@ public class Main {
 		AuthorRepository authorRepository = new AuthorRepository(entityManager);
 		// Create an author
 		//Author virginia = new Author("Virginia Wolf", "UK");
-		//Author leon = new Author("Leon Tolstoi", "Russia");
+		Author leon = new Author("Leon Tolstoi", "Russia");
 		//Author victor = new Author("Victor Hugo", "French");
 		//Author dante = new Author("Dante Alighieri", "Italy");
 		//Author tolkien = new Author("J.R.R.Tolkien", "UK");
 
 		//Optional<Author> savedAuthor1 = authorRepository.save(virginia);
-		//Optional<Author> savedAuthor2 = authorRepository.save(leon);
+		Optional<Author> savedAuthor2 = authorRepository.save(leon);
 		//Optional<Author> savedAuthor3 = authorRepository.save(victor);
 		//Optional<Author> savedAuthor4 = authorRepository.save(dante);
 		//Optional<Author> savedAuthor5 = authorRepository.save(tolkien);
 		
-		String name;
-		String country;
-		while (true) {
-		System.out.println("Insert name of author(type quit to exit): ");
-		String optionName = reader.nextLine();
-		if (optionName.equals("quit")) {
-			break;
-		}
-		System.out.println("Insert country of author: ");
-		String optionCountry = reader.nextLine();
-		Author testAuthor = new Author(optionName, optionCountry);
-		Optional<Author> savedAuthor = authorRepository.save(testAuthor);
-		}
+		
+		 String name; 
+		 String country; 
+		 while (true) {
+		  
+			 System.out.println("Insert name of author(type quit to exit): "); 
+		  
+			 String optionName = reader.nextLine(); 
+			 if (optionName.equals("quit")) { 
+				 break;
+			 }
+		  
+			 System.out.println("Insert country of author: "); 
+		 
+			 String optionCountry = reader.nextLine(); 
+			 Author testAuthor = new Author(optionName, optionCountry);
+			 if(authorRepository.findByName(testAuthor.getName()).equals(testAuthor.getName())){
+				 
+				 Optional<Author> savedAuthor = authorRepository.save(testAuthor);
+			 }
+		 }	 
 		
 		
 		// System.out.println("Saved author: " + savedAuthor1.get());
