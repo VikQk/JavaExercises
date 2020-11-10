4c994f55cba6412d5f0bfc00ecfce935291405fd
mati <mati@DESKTOP-GAKM3OF>
2020-11-10 13:22:43 +0100

    commit 10/11/2020

---
+++ SpringLibrary/src/main/java/com/springbootthymeleaf/DemoSpringWebApplication.java
@@ -0,0 +1,9 @@
+package com.springbootthymeleaf;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DemoSpringWebApplication {
+public static void main(String[] args) {
+SpringApplication.run(DemoSpringWebApplication.class, args);
+}
+}
