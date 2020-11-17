fd377e596080ea2ec4b2ac876660ea45dec19263
mati <mati@DESKTOP-GAKM3OF>
2020-11-17 13:39:28 +0100

    commit1

---
+++ shopExemple/src/main/java/com/example/shopExemple/ShopExempleApplication.java
@@ -0,0 +1,12 @@
+package com.example.shopExemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopExempleApplication.class, args);
+	}
+}
---
+++ shopExemple/src/main/java/com/example/shopExemple/boot/controller/CreditCardController.java
@@ -0,0 +1,22 @@
+package com.example.shopExemple.boot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.shopExemple.boot.service.CreditCardService;
+
+@Controller
+@RequestMapping("/creditcards")
+public class CreditCardController {
+
+	@Autowired
+	CreditCardService service;
+
+	@RequestMapping("/show")
+	public String showCreditCards (Model model) {
+
+		model.addAttribute("cards", service.findAll());
+		return "shoping/cards.html";
+	}
+}
