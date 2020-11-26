4b39d4d69f5878d39ef307b1913b951e4edd5039
mati <mati@DESKTOP-GAKM3OF>
2020-11-26 13:54:49 +0100

    actualizacion 26/11/2020

--- SpringLibrary/src/main/java/com/example/SpringLibrary/boot/controller/UserController.java
+++ SpringLibrary/src/main/java/com/example/SpringLibrary/boot/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.example.SpringLibrary.boot.controller;
 
-import javax.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.example.SpringLibrary.boot.model.User;
 import com.example.SpringLibrary.boot.service.UserService;
 
@@ -34,8 +31,6 @@ public class UserController {
 	@RequestMapping("/insertUser")
 	public String insertUser(User user, Model model) {
 		
-		System.out.println(model.getAttribute(user.getLastName()));
-		System.out.println(user.getName());
 		service.insertUser(user);
 		model.addAttribute("user",service.findAll());
 		return "redirect:/user/show";
